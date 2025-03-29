3480e4ef6efbf855102222b69d2071353a0e677e
Benito <39175198+benitopatino@users.noreply.github.com>
2025-03-29 11:46:21 -0700

    save

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginFormComponent } from './login/login-form/login-form.component';
+import { NewsfeedComponent } from './newsfeed/newsfeed/newsfeed.component';
 export const routes: Routes = [
 
-    {path:'login',component: LoginFormComponent}
+    { path: 'login', component: LoginFormComponent },
+    { path: 'home', component: NewsfeedComponent }
 ];
