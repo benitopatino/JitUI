1e5cc75a9953ef23619dca02a71df6ac3f393e9a
Benito <39175198+benitopatino@users.noreply.github.com>
2025-03-28 20:04:58 -0700

    Fix interceptor

--- src/app/login/login-service/login.service.ts
+++ src/app/login/login-service/login.service.ts
@@ -17,7 +17,7 @@ const httpOptions = {
 export class LoginService {
 
   private API_URL: string = 'http://localhost:5073/api/auth/login'
-
+  static readonly JitTokenSessionName: string = 'jitAuthToken';
   constructor(private http: HttpClient) { }
 
   login(login: Login): Observable<AuthResponse> {
@@ -25,9 +25,14 @@ export class LoginService {
       .pipe(
         tap((response: AuthResponse) => {
           if (response.isAuthenticated && response.token)
-            localStorage.setItem('jitAuthToken', response.token);
+            localStorage.setItem(LoginService.JitTokenSessionName, response.token);
         })
       );
+  }
+
+
+  getToken(): string | null {
+    return localStorage.getItem(LoginService.JitTokenSessionName);
   }
 
 
