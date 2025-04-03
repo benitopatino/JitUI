3bbf81f99b0a6e9e984e6a1dba7673d43eb1e696
Benito <39175198+greatfierro@users.noreply.github.com>
2025-04-02 19:08:18 -0700

    Add register model to registration component

--- src/app/register/register-form/register-form.component.ts
+++ src/app/register/register-form/register-form.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Login } from '../../login/models/login';
+import { Register } from '../models/register';
+import { RegisterService } from '../service/register.service';
 
 @Component({
   selector: 'app-register-form',
@@ -6,6 +9,15 @@ import { Component } from '@angular/core';
   templateUrl: './register-form.component.html',
   styleUrl: './register-form.component.css'
 })
+
 export class RegisterFormComponent {
+  register: Register = {
+
+    firstname: '',
+    lastname: '',
+    email: '',
+    password:'',
+  }
+
 
 }
