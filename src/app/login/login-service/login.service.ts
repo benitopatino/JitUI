eb85fa5591847e1d57333368a0d12016f1634340
Benito <39175198+greatfierro@users.noreply.github.com>
2025-04-16 19:51:36 -0700

    add login dto

--- src/app/login/login-service/login.service.ts
+++ src/app/login/login-service/login.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, tap } from 'rxjs';
 import { Login } from '../models/login';
 import { AuthResponse } from '../models/auth-response';
+import { LoginDTO } from '../models/loginDTO';
 const httpOptions = {
   headers: new HttpHeaders({
     'Content-Type': 'application/json',
@@ -16,12 +17,19 @@ const httpOptions = {
 
 export class LoginService {
 
+  dto: LoginDTO = {
+    username: '',
+    password: ''
+  }
+
   private API_URL: string = 'http://localhost:5073/api/auth/login'
   static readonly JitTokenSessionName: string = 'jitAuthToken';
   constructor(private http: HttpClient) { }
 
   login(login: Login): Observable<AuthResponse> {
-    return this.http.post<AuthResponse>(this.API_URL, login, httpOptions)
+    this.dto.username = login.email;
+    this.dto.password = login.password;
+    return this.http.post<AuthResponse>(this.API_URL, this.dto, httpOptions)
       .pipe(
         tap((response: AuthResponse) => {
           if (response.isAuthenticated && response.token)
