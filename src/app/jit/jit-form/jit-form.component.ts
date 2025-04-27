ea3ed2fca92cfdacfa61317e93ba08e620feb9f8
Benito <39175198+greatfierro@users.noreply.github.com>
2025-04-27 13:04:47 -0700

    basic wiring of jit form

--- src/app/jit/jit-form/jit-form.component.ts
+++ src/app/jit/jit-form/jit-form.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { Jit } from '../models/jit';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-jit-form',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './jit-form.component.html',
   styleUrl: './jit-form.component.css'
 })
 export class JitFormComponent {
 
+  jit: Jit = {
+    content:''
+  }
+
+  onSubmit(myForm: NgForm): void{
+    console.log(this.jit);
+  }
 }
