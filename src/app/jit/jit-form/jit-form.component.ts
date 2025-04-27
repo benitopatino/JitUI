49b2304d5188b5e0b48a426815f2ac6b00ea05c0
Benito <39175198+greatfierro@users.noreply.github.com>
2025-04-26 20:17:26 -0700

    init commit added component

---
+++ src/app/jit/jit-form/jit-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-jit-form',
+  imports: [],
+  templateUrl: './jit-form.component.html',
+  styleUrl: './jit-form.component.css'
+})
+export class JitFormComponent {
+
+}
