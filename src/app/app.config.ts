b3ce81f39049a8c1e2de6dc75d4f754dad6942ed
Benito Patino <39175198+benitopatino@users.noreply.github.com>
2025-06-17 23:42:41 -0700

    add JwtHelperService and JwtModule to Login.Service.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,20 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptors } from '@angular/common/http';
 import { tokenInterceptorFn } from './login/interceptor/token.interceptor';
+import { JwtModule } from '@auth0/angular-jwt';
+import { LoginService } from './login/login-service/login.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
-  provideHttpClient(withInterceptors([tokenInterceptorFn]))],
-};
+  provideHttpClient(withInterceptors([tokenInterceptorFn])), importProvidersFrom(
+            JwtModule.forRoot({
+                config: {
+                    allowedDomains: ["example.com"],
+                    disallowedRoutes: ["http://example.com/examplebadroute/"],
+                },
+            }),
+        ),
+]};
