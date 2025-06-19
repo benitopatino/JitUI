82a0cecf3f29431779cfd3a10403654e75bbc8b0
Benito <39175198+greatfierro@users.noreply.github.com>
2025-06-18 19:15:13 -0700

    pass login service and import common module to navbar.component.ts

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { LoginService } from '../login/login-service/login.service';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink,RouterOutlet],
+  imports: [RouterLink,RouterOutlet, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+
+  constructor(public loginService: LoginService){}
 
 }
