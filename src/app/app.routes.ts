60264c38fa31a7e1482f7a1e2f425b6ecbda354e
Benito <39175198+greatfierro@users.noreply.github.com>
2025-06-18 19:45:04 -0700

    apply authGuard to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,13 +5,14 @@ import { NewsfeedService } from './newsfeed/service/newsfeed.service';
 import { RegisterFormComponent } from './register/register-form/register-form.component';
 import { UserProfileComponent } from './profile/user-profile/user-profile.component';
 import { LogoutComponent } from './logout/logout.component';
+import { authGuard } from './auth.guard';
 export const routes: Routes = [
 
     { path: 'login', component: LoginFormComponent },
     { path: 'register', component: RegisterFormComponent },
-    { path: 'logout', component: LogoutComponent},
-    { path: 'profile', component: UserProfileComponent },
-    { path: '', component: NewsfeedComponent },
-    { path: ':username', component: UserProfileComponent},
+    { path: 'logout', component: LogoutComponent, canActivate:[authGuard]},
+    { path: 'profile', component: UserProfileComponent,canActivate:[authGuard] },
+    { path: '', component: NewsfeedComponent,canActivate:[authGuard] },
+    { path: ':username', component: UserProfileComponent, canActivate:[authGuard]},
 
 ];
