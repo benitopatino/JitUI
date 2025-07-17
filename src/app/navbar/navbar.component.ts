e876bb9a29da0de3f7acf9fee7ca50ebabed8e39
Benito Patino <39175198+benitopatino@users.noreply.github.com>
2025-07-16 20:54:27 -0700

    add styling and logic to show search user results

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,14 +2,43 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { LoginService } from '../login/login-service/login.service';
 import { CommonModule } from '@angular/common';
+import { UserProfile } from '../profile/model/userProfile';
+import { FormsModule } from '@angular/forms';
+import { UserProfileService } from '../profile/service/user-profile.service';
+import { UserProfileSearchDTO } from '../profile/model/userProfileSearchDTO';
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, CommonModule],
+  imports: [RouterLink, CommonModule, FormsModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
 
-  constructor(public loginService: LoginService){}
+  searchQuery: string = '';
+  results: UserProfileSearchDTO[] = [];
+  isFocused = false;
+  blurTimeout: any;
+
+  constructor(public loginService: LoginService, private userProfileService: UserProfileService){}
+
+  onSearch(){
+    console.log(this.searchQuery)
+    this.userProfileService.searchUserProfiles(this.searchQuery)
+    .subscribe(     {
+            next: (res) => {this.results = res},
+            error: err => { console.log("erersr")}
+          })
+  }
+  onBlur() {
+  // Delay hiding so click events like (mousedown) still register
+  this.blurTimeout = setTimeout(() => this.isFocused = false, 200);
+}
+
+selectResult(item: UserProfileSearchDTO) {
+  // Navigate to user profile, etc.
+  console.log('Selected:', item);
+  clearTimeout(this.blurTimeout);
+  this.isFocused = false;
+}
 
 }
---
+++ src/app/profile/model/userProfileSearchDTO.ts
@@ -0,0 +1,6 @@
+export interface UserProfileSearchDTO {
+    firstName: string;
+    lastName: string;
+    username: string;
+    avatarUrl: string;
+}
